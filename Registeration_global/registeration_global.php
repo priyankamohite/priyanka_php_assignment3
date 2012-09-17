2b119ca2935c2b42ef82f7866fb2632d8d82115a
webonise <webonise@webonise-desktop.(none)>
2012-09-17 17:55:26 +0530

    made corrections

---
+++ Registeration_global/registeration_global.php
@@ -0,0 +1,13 @@
+<?php
+
+	$user_name=$_GET['username'];
+	$user_password=$_GET['password'];
+	echo "User name is:$use_rname <br/> Password is:$user_password<br/>";
+
+	$user_name=$_GET['username'];
+	$user_password=$_GET['password'];
+	echo "User name is:$use_rname <br/> Password is:$user_password<br/>";
+
+
+?>
+
